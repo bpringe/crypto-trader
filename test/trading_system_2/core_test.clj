f3a22d61e2782517b71218288c2c800815599be9
Brandon Ringe <brandon.ringe@gmail.com>
2017-12-09 09:18:01 -0600

    Get sha512-hmac request signing working and create get-balances request

---
+++ test/trading_system_2/core_test.clj
@@ -0,0 +1,7 @@
+(ns crypto-trader.core-test
+  (:require [clojure.test :refer :all]
+            [crypto-trader.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
