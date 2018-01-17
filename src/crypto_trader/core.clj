867153b02aa97941f6892103490549e517d78a64
Brandon Ringe <brandon.ringe@gmail.com>
2018-01-17 13:17:43 -0600

    Add further attempt at signing requests

--- src/crypto_trader/core.clj
+++ src/crypto_trader/core.clj
@@ -91,24 +91,32 @@
       (-> hmac
           .getBytes
           b64/encode
-          (String. "UTF-8")))))
-
-(defn- make-signed-request 
+          String.))))
+      
+(defn- send-signed-request 
   [method path & [opts]]
-  (let [timestamp (long (:epoch (get-time)))
+  (let [url (str (:api-base-url config) path)
+        timestamp (long (:epoch (get-time)))
         signature (create-signature timestamp method path (:body opts))]
     (http/request
       (merge {:method method
-              :url (str (:api-base-url config) path)
+              :url url
               :as :json
-              :headers {:CB-ACCESS-KEY (:api-key config)
-                        :CB-ACCESS-SIGN signature
-                        :CB-ACCESS-TIMESTAMP timestamp
-                        :CB-ACCESS-PASSPHRASE (:api-passphrase config)}}
+              :headers {"CB-ACCESS-KEY" (:api-key config)
+                        "CB-ACCESS-SIGN" signature
+                        "CB-ACCESS-TIMESTAMP" timestamp
+                        "CB-ACCESS-PASSPHRASE" (:api-passphrase config)
+                        "Content-Type" "application/json"}
+              :debug true}
              opts))))
              
 (defn get-accounts []
-  (make-signed-request "GET" "/accounts"))
+  (send-signed-request "GET" "/accounts"))
+  
+(send-signed-request "GET" "/accounts")
+
+; (def secret-decoded (String. (b64/decode (.getBytes (:api-secret config)))))
+; (sha256-hmac "1516214750GET/fills" secret-decoded)
 
 (defn -main
   "I don't do a whole lot ... yet."
